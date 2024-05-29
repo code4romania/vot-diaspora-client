d26a5058c13001c53e73f3ec263fed24ccffb9f9
Andrei Ioniță <hi@andrei.io>
2024-05-29 20:27:32 +0100

    update favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,7 +47,25 @@ export default {
         rel: 'stylesheet',
         href: 'https://js.api.here.com/v3/3.1/mapsjs-ui.css',
       },
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.jpg' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png',
+      },
+      { rel: 'manifest', href: '/site.webmanifest' },
     ],
     script: [
       {
